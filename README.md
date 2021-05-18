e226a83591097c35cc3314eaa2f6fc692cb87c24
Achivhane <asmita12016@gmail.com>
2021-05-18 15:39:47 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-fcb7cv
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-fcb7cv)
---
+++ assignment 2/src/app/advertisement-table/advertisement-table.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
+import { AdvertisementServiceService } from '../advertisement-service.service';
+import { SearchFilterPipe } from '../search-filter.pipe';
+
+@Component({
+  selector: 'app-advertisement-table-component',
+  templateUrl: './advertisement-table.component.html',
+  styleUrls: ['./advertisement-table.component.css'],
+  providers: [SearchFilterPipe]
+})
+export class AdvertisementTableComponentComponent implements OnInit {
+  //@Input('adList') ads;
+  ads: any[];
+  searchString: string;
+  constructor(
+    private router: Router,
+    private searchFilter: SearchFilterPipe,
+    private _adService: AdvertisementServiceService
+  ) {
+    this.ads = _adService.getAdvertisements();
+  }
+
+  ngOnInit(): void {}
+
+  deleteAd(ad) {
+    this._adService.deleteAdvertisement(ad);
+  }
+
+  editAd(ad) {
+    this.router.navigate(['edit', ad.title.toString()]);
+  }
+}
